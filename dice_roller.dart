9b267ba63d21f33f0e9e6fece3e4696ccd6a1c9c
Aman89584 <142104494+Aman89584@users.noreply.github.com>
2023-08-22 21:55:14 +0530

    Add files via upload

---
+++ dice_roller.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import "dart:math";
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  // final randomizer = Random();
+  var currentDiceRoll = 2;
+
+  // var activeDiceImage = 'assets/images/dice-1.png';
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1; // 1<>6
+      // activeDiceImage = 'assets/images/dice-$diceRoll.png';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 150,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              // padding: const EdgeInsets.only(
+              //   top: 20,
+              // ),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 28,
+              ),
+            ),
+            child: const Text('Roll Dice'))
+      ],
+    );
+  }
+}
---
+++ gradient_container.dart
@@ -0,0 +1,74 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+  
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [color1, color2], begin: startAlignment, end: endAlignment),
+      ),
+      child: const Center(
+          child: DiceRoller()
+      ),
+    );
+  }
+} 
+
+
+
+
+
+
+//---------------first approcah to pass color as argument------------------
+//   final List<Color> colors;
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//         colors: colors, 
+//         begin: startAlignment,
+//         end: endAlignment),
+//       ),
+//       child: const Center(
+//         child: StyleText('RecCloth'),
+//       ),
+//     );
+//   }
+// } 
+
+
+
+
+
+
+
+//-----------------second approch--------------------
+
+//   final List<Color> colors;
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//         colors: colors, 
+//         begin: startAlignment,
+//         end: endAlignment),
+//       ),
+//       child: const Center(
+//         child: StyleText('RecCloth'),
+//       ),
+//     );
+//   }
+// }
